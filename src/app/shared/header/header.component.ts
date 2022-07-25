a49e62118d559a9f026366d241164003a9d0b0fb
DaniRicher <richerdani3@gmail.com>
2022-07-25 01:09:57 -0500

    Boton finish

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -11,6 +12,17 @@ export class HeaderComponent implements OnInit {
   constructor( private router: Router ) { }
 
   ngOnInit(): void {
+  }
+  finalizarApp() {
+    Swal.fire({
+      title: '¿Seguro que desea terminar la aplicación?',
+      showCancelButton: true,
+    }).then((result) => {
+
+      if ( result.isConfirmed ) {
+        localStorage.clear();;
+      } 
+    })
   }
 
 }
